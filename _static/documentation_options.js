c49320f18e324fbe383c5728fd9af6fda0b1446a
Adrian Liaw <adrianliaw2000@gmail.com>
2018-08-22 09:28:36 +0000

    Build 201808221728

---
+++ _static/documentation_options.js
@@ -0,0 +1,9 @@
+var DOCUMENTATION_OPTIONS = {
+    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
+    VERSION: '3.7.0',
+    LANGUAGE: 'zh_TW',
+    COLLAPSE_INDEX: false,
+    FILE_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt'
+};
